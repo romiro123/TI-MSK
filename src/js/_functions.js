b36238b03f183c476e926c34cf9741ec58b3cb26
Киленников Роман <kilennikovroman@gmail.com>
2023-10-24 10:07:39 +0300

    add aos

--- src/js/_functions.js
+++ src/js/_functions.js
@@ -38,8 +38,8 @@ import { burger } from './functions/burger';
 // });
 
 // Подключение анимаций по скроллу
-// import AOS from 'aos';
-// AOS.init();
+import AOS from 'aos';
+AOS.init();
 
 // Подключение параллакса блоков при скролле
 // import Rellax from 'rellax';
